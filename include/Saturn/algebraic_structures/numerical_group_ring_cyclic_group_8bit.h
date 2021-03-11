232f1ccce35592a8b932468ad537b6a05238ff3d
define1111 <s.vershkov.def@yandex.ru>
2021-03-11 10:35:09 +0300

    Added numerical group ring of cyclic group 8bit

---
+++ include/Saturn/algebraic_structures/numerical_group_ring_cyclic_group_8bit.h
@@ -0,0 +1,20 @@
+#ifndef _SENTRY_SATURN_NUMERICAL_GROUP_RING_CYCLIC_GROUP_8BIT_H_
+#define _SENTRY_SATURN_NUMERICAL_GROUP_RING_CYCLIC_GROUP_8BIT_H_
+
+/* Numerical group ring of cyclic group is Z_m[Z_n]. */
+
+typedef struct numerical_group_ring_cyclic_group_8bit_tag
+{
+    unsigned char ring_order;
+    unsigned char group_order;
+    unsigned int group_ring_order;
+    unsigned char **elements;
+} n_gr_ccl_8b_t;
+
+n_gr_ccl_8b_t init_n_gr_ccl_8b(unsigned char ring_order, unsigned char group_order);
+void print_n_gr_ccl_8b(n_gr_ccl_8b_t *group_ring);
+/* temp void */ 
+void multy_elements_n_gr_ccl_8b(n_gr_ccl_8b_t *group_ring, unsigned int x, unsigned int y);
+void free_n_gr_ccl_8b(n_gr_ccl_8b_t *group_ring);
+
+#endif
---
+++ src/algebraic_structures/numerical_group_ring_cyclic_group_8bit.c
@@ -0,0 +1,110 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <math.h>
+
+/*#include "../../include/Saturn/algebraic_structures/permutation.h"*/
+
+#include "../../include/Saturn/algebraic_structures/numerical_group_ring_cyclic_group_8bit.h"
+
+/*permutation_t *sigma_arr = NULL;*/
+
+n_gr_ccl_8b_t
+init_n_gr_ccl_8b(unsigned char ring_order, unsigned char group_order)
+{
+    n_gr_ccl_8b_t group_ring;
+
+    group_ring.ring_order = ring_order;
+    group_ring.group_order = group_order;
+    group_ring.group_ring_order = pow(group_ring.ring_order, group_ring.group_order);
+
+    group_ring.elements = (unsigned char **) malloc(group_ring.group_ring_order * sizeof(unsigned char *));
+
+    for (unsigned int i = 0; i < group_ring.group_ring_order; ++i)
+        group_ring.elements[i] = (unsigned char *) malloc(group_ring.group_order * sizeof(unsigned char));
+
+    for (unsigned int j = 0; j < group_ring.group_order; ++j) 
+    {
+        group_ring.elements[0][j] = 0;   
+    }
+    
+    for (unsigned int i = 1; i < group_ring.group_ring_order; ++i) 
+    {
+        unsigned int rem = 0;
+        
+        for (unsigned int j = 0; j < group_ring.group_order; ++j) 
+        {
+            unsigned char value = group_ring.elements[i - 1][j] + rem;
+            
+            if (j == 0) 
+            {
+                value += 1;
+            }
+            if (value >= group_ring.ring_order) 
+            {
+                rem = 1;
+                value = 0;
+            } else 
+            {
+                rem = 0;
+            }
+            
+            group_ring.elements[i][j] = value;
+        }
+    }
+
+    return group_ring;
+}
+
+void
+print_n_gr_ccl_8b(n_gr_ccl_8b_t *group_ring)
+{
+    for (unsigned int i = 0; i < group_ring->group_ring_order; ++i) 
+    {
+        printf("%d: ", i);
+        
+        for (unsigned int j = 0; j < group_ring->group_order; ++j)  
+        {
+            printf("%d ", group_ring->elements[i][j]);
+        }
+        
+        printf("\n");
+    }
+}
+
+void /* temp void */
+multy_elements_n_gr_ccl_8b(n_gr_ccl_8b_t *group_ring, unsigned int x, unsigned int y)
+{
+    unsigned char result[group_ring->group_order];
+
+    for (unsigned int i = 0; i < group_ring->group_order; ++i)
+    {
+        unsigned char coef = 0;
+
+        for (unsigned int j = 0; j < group_ring->group_order; ++j)
+        {
+            /*coef += group_ring->elements[x][j]*group_ring->elements[y][sigma_arr[i].p[j]];*/
+            coef += group_ring->elements[x][j]*group_ring->elements[y][((i - j) + group_ring->ring_order) % group_ring->ring_order];
+        }
+
+        result[i] = coef % group_ring->ring_order;
+    }
+
+    /*printf("\n");
+
+    for (unsigned int i = 0; i < group_ring->group_order; ++i)
+    {
+        printf("%u ", nll[i]);
+    }
+
+    printf("\n");*/
+}
+
+void
+free_n_gr_ccl_8b(n_gr_ccl_8b_t *group_ring)
+{
+    for (unsigned int i = 0; i < group_ring->group_ring_order; ++i)
+        free(group_ring->elements[i]);
+    
+    free(group_ring->elements);
+}
